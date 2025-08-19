17724506e85acbdb7c0198e1b075c551cc54ff83
Andrew Wilkinson <andrewjwilkinson@gmail.com>
2025-08-19 21:34:06 +0100

    Switch to using soco events so Sonos data is always up to date.

--- smartdisplay/sonos.py
+++ smartdisplay/sonos.py
@@ -4,26 +4,168 @@ from datetime import datetime
 from functools import lru_cache
 import io
 import math
+import queue
 import struct
 import sys
+import threading
+import time
 from typing import Any, Dict, Optional, List
 from xml.dom.minidom import parseString
 
 from PIL import Image
 import requests
+
+from sentry_sdk import capture_exception  # type:ignore
 import soco  # type: ignore
+
+# Apple Music
+# {'creator': 'Arcade Fire', 'stream_content': '', 'radio_show': '',
+#  'album_art_uri': '/getaa?s=1&u=xyz',
+#  'album': 'Funeral', 'parent_id': '-1', 'item_id': '-1', 'restricted': True,
+#  'title': 'Neighborhood #1 (Tunnels)',
+#  'resources': [{'uri': 'x-sonos-http:song%3a1249418558.mp4?...',
+#                'protocol_info': 'sonos.com-http:*:audio/mp4:*',
+#                'import_uri': None, 'size': None, 'duration': '0:04:48',
+#                'bitrate': None, 'sample_frequency': None,
+#                'bits_per_sample': None, 'nr_audio_channels': None,
+#                'resolution': None, 'color_depth': None, 'protection': None}],
+#  'desc': None}
+
+# BBC Sounds
+# {'stream_content': "BR P|TYPE=SNG|TITLE (Are You Ready) Do the Bus Stop "
+#                    "(Bustin' Loose Disco Express Remix)|ARTIST Fatback Band"
+#                    "|ALBUM ",
+#  'radio_show': 'Radio 6 Music,',
+#  'album_art_uri': 'https://ichef.bbci.co.uk/images/ic/640x640/p0bqcdzf.jpg',
+#  'parent_id': '-1', 'item_id': '-1', 'restricted': True, 'title': '',
+#  'resources': [{'uri': 'x-sonosapi-hls:stations%7...',
+#                'protocol_info': 'sonos.com-http:*:application/x-mpegURL:*',
+#                'import_uri': None, 'size': None,
+#                'duration': None, 'bitrate': None, 'sample_frequency': None,
+#                'bits_per_sample': None, 'nr_audio_channels': None,
+#                'resolution': None, 'color_depth': None, 'protection': None}],
+#  'desc': None}
+
+# Music Library
+# {'creator': '86TVs', 'stream_content': '', 'radio_show': '',
+#  'album_art_uri': '/getaa?u=x-file-cifs%3a%2f%2f192.168.1.207%2f...',
+#  'album': '86TVs', 'original_track_number': 1, 'parent_id': '-1',
+#  'item_id': '-1', 'restricted': True, 'title': 'Modern Life',
+#  'resources': [{'uri': 'x-file-cifs://192.168.1.207/music_hq/...',
+#                 'protocol_info': 'x-file-cifs:*:audio/flac:*',
+#                 'import_uri': None, 'size': None,
+#                 'duration': '0:03:11', 'bitrate': None,
+#                 'sample_frequency': None,
+#                 'bits_per_sample': None, 'nr_audio_channels': None,
+#                 'resolution': None,
+#                 'color_depth': None, 'protection': None}], 'desc': None}
+
+CURRENT_TRACK_INFO: Optional["TrackInfo"] = None
+
+
+class Terminator:
+    def __init__(self) -> None:
+        self._terminate = False
+
+    def terminate(self) -> None:
+        self._terminate = True
+
+    def is_terminated(self) -> bool:
+        return self._terminate
+
+
+def sonos_watcher(handler: "SonosHandler", terminator: Terminator) -> None:
+    devices = {}
+    subscription = None
+    try:
+        while "Kitchen" not in devices:
+            devices = {device.player_name: device
+                       for device in soco.discover(timeout=60)}
+
+        subscription = soco.services.AVTransport(devices["Kitchen"]) \
+            .subscribe(auto_renew=True)
+
+        while not terminator.is_terminated():
+            try:
+                event = subscription.events.get(timeout=5)
+
+                if event.variables.get("transport_state", None) != "PLAYING":
+                    handler.track_info = None
+                    continue
+                if "current_track_meta_data" in event.variables \
+                        and event.variables["current_track_meta_data"] != "":
+                    track_info = process_event_track_metadata(
+                        event.variables["current_track_meta_data"].to_dict(),
+                        devices["Kitchen"])
+                    handler.track_info = track_info
+                    print(track_info)
+            except queue.Empty:
+                pass
+    except Exception as e:
+        sys.stderr.write("Error in sonos_watcher:\n")
+        sys.stderr.write(repr(e) + "\n")
+        sys.stderr.flush()
+        capture_exception(e)
+    finally:
+        handler.track_info = None
+        if subscription is not None:
+            try:
+                subscription.unsubscribe()
+            except Exception as e:
+                pass
+
+
+def stream_content_split(stream_content: str, key: str) -> str:
+    if key in stream_content:
+        return stream_content.split(key)[1].split("|")[0]
+    return ""
+
+
+def process_event_track_metadata(metadata: Dict[str, Any],
+                                 device: Any) -> Optional["TrackInfo"]:
+    if "creator" in metadata and len(metadata["creator"]) > 0:
+        return TrackInfo({
+            "artist": metadata["creator"],
+            "album": metadata.get("album", ""),
+            "title": metadata.get("title", ""),
+            "album_art": metadata.get("album_art_uri", "")
+        }, device.ip_address)
+    elif "stream_content" in metadata and len(metadata["stream_content"]) > 0:
+        return TrackInfo({
+            "artist": stream_content_split(
+                metadata["stream_content"],
+                "|ARTIST "),
+            "album": stream_content_split(
+                metadata["stream_content"],
+                "|ALBUM "),
+            "title": stream_content_split(
+                metadata["stream_content"],
+                "|TITLE "),
+            "album_art": metadata.get("album_art_uri", "")
+        }, device.ip_address)
+    elif "title" in metadata and len(metadata["title"]) > 0:
+        return TrackInfo({
+            "artist": "",
+            "album": "",
+            "title": metadata["title"],
+            "album_art": metadata.get("album_art_uri", "")
+        }, device.ip_address)
+    else:
+        sys.stderr.write("Unknown metadata format:\n")
+        sys.stderr.write(repr(metadata) + "\n")
+        return None
 
 
 class TrackInfo:
-    def __init__(self, track_info: Any, sonos: "SonosHandler") -> None:
+    def __init__(self, track_info: Any, sonos_uri: str) -> None:
         self.created = datetime.utcnow()
         self.artist = track_info["artist"]
         self.album = track_info["album"]
         self.title = track_info["title"]
         self.album_art = track_info["album_art"]
-        print(repr(self.album_art))
-        if self.album_art is None or len(self.album_art) == 0:
-            self.album_art = sonos.fix_album_art_url(track_info)
+        if not self.album_art.startswith("http"):
+            self.album_art = f"http://{sonos_uri}:1400{self.album_art}"
+
         if self.album_art is not None and len(self.album_art) > 0:
             try:
                 self.album_art_header: Optional[bytes] = \
@@ -32,7 +174,7 @@ class TrackInfo:
                     get_album_art(self.album_art, False)
             except requests.exceptions.ConnectionError as e:
                 sys.stderr.write("Error loading Album Art\n")
-                sys.stderr.write(str(e))
+                sys.stderr.write(repr(e))
                 self.album_art_header = None
                 self.album_art_image = None
         else:
@@ -52,55 +194,40 @@ class TrackInfo:
         return not (self == other)
 
     def __str__(self):
-        return f"<TrackInfo {self.artist} {self.title}" + \
+        return f"<TrackInfo '{self.artist}' '{self.album}' '{self.title}'" + \
                f" {self.album_art_image is not None}>"
 
 
 class SonosHandler:
     def __init__(self) -> None:
-        self.devices: Dict[str, Any] = {}
+        self._last_track_info: Optional[TrackInfo] = None
         self.track_info: Optional[TrackInfo] = None
         self.last_screen = "sonos"
-        self.last_display_time: Optional[datetime] = None
+        self._last_display_time: Optional[datetime] = None
 
-    def _get_current_track_info(self) -> Optional[TrackInfo]:
-        target = self.get_sonos_device()
-        if target is None:
-            return None
-        transport_info = target.group.coordinator.get_current_transport_info()
-        if transport_info['current_transport_state'] != "PLAYING":
-            return None
+        self._terminator = Terminator()
+        self._thread = threading.Thread(target=sonos_watcher,
+                                        args=(self, self._terminator))
+        self._thread.daemon = True
+        self._thread.start()
 
-        try:
-            return TrackInfo(target.group.coordinator.get_current_track_info(),
-                             self)
-        except requests.exceptions.ReadTimeout:
-            return None
-
-    def get_sonos_device(self) -> Optional[Any]:
-        if "Kitchen" not in self.devices:
-            self.devices = {device.player_name: device
-                            for device in soco.discover(timeout=60)}
-
-        try:
-            return self.devices["Kitchen"]
-        except KeyError:
-            sys.stderr.write("No device Kitchen\n")
-            return None
+    def __del__(self):
+        self._terminator.terminate()
+        if self._thread.is_alive():
+            self._thread.join(timeout=5)
 
     def has_track_changed(self) -> bool:
-        new_info = self._get_current_track_info()
-        if new_info is None:
-            self.track_info = None
+        if self.track_info is None:
             self.last_display_time = None
+            self._last_track_info = None
             return False
-        if self.track_info is None or self.track_info != new_info:
-            self.track_info = new_info
+        if self._last_track_info is None \
+                or self.track_info != self._last_track_info:
+            self._last_track_info = self.track_info
             self.last_display_time = datetime.utcnow()
             return True
-        if abs((new_info.created - self.track_info.created).total_seconds()) \
-           > 60 * 5:
-            self.track_info = new_info
+        gap = self.track_info.created - self._last_track_info.created
+        if abs(gap.total_seconds()) > 60 * 5:
             self.last_display_time = datetime.utcnow()
             return True
         return False
@@ -111,23 +238,6 @@ class SonosHandler:
         return self.track_info.album_art_header if header \
             else self.track_info.album_art_image
 
-    def fix_album_art_url(self, track_info) -> Optional[str]:
-        if "metadata" not in track_info:
-            return None
-
-        xml = parseString(track_info["metadata"])
-        tags = xml.getElementsByTagName("upnp:albumArtURI")
-        if len(tags) == 0:
-            return None
-
-        url = xml_get_text(tags[0].childNodes)
-
-        if not url.startswith("http"):
-            device = self.get_sonos_device()
-            if device is not None:
-                url = f"http://{device.ip_address}{url}"
-        return url
-
     def set_last_screen(self, screen: str) -> None:
         self.last_screen = screen
 
@@ -135,11 +245,11 @@ class SonosHandler:
         return self.last_screen
 
     def show_quick(self) -> bool:
-        if self.last_display_time is None:
+        if self._last_display_time is None:
             return False
-        if (datetime.utcnow() - self.last_display_time).total_seconds() \
+        if (datetime.utcnow() - self._last_display_time).total_seconds() \
            > 2 * 60:
-            self.last_display_time = datetime.utcnow()
+            self._last_display_time = datetime.utcnow()
             return True
         return False
 
@@ -200,7 +310,7 @@ def get_album_art(art_uri: str, header: bool) -> Optional[bytes]:
                 image_data.extend([0, 0, 0] * xafter)
         if yafter > 0:
             image_data.extend([0, 0, 0] * 64 * yafter)
-
+    sys.stdout.write(f"Album art size: {len(image_data)}\n")
     return struct.pack("B" * (expected_size), *image_data)
 
 
@@ -213,4 +323,16 @@ def xml_get_text(nodelist):
 
 
 if __name__ == "__main__":
-    SonosHandler()._get_current_track_info()
+    import time
+    handler = SonosHandler()
+
+    while True:
+        if handler.has_track_changed():
+            print("Track changed:", handler.track_info)
+        elif handler.show_quick():
+            print("Show quick:", handler.track_info)
+        else:
+            print("No change:", handler.track_info)
+
+        sys.stdout.flush()
+        time.sleep(5)
