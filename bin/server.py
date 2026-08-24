00573159f7840856d01c618be84421e89ef93465
Andrew Wilkinson <andrewjwilkinson@gmail.com>
2026-08-24 21:54:17 +0100

    Handle address already in use errors.

--- bin/server.py
+++ bin/server.py
@@ -16,10 +16,16 @@ def main(port) -> None:
         profiles_sample_rate=0.0,
     )
 
-    with socketserver.TCPServer(("", port), SmartDisplayHandler) as httpd:
-        httpd.allow_reuse_address = True
-        print("serving at port", port)
-        httpd.serve_forever()
+    while True:
+        try:
+            with socketserver.TCPServer(("", port), SmartDisplayHandler) as httpd:
+                httpd.allow_reuse_address = True
+                print("serving at port", port)
+                httpd.serve_forever()
+        except OSError as e:
+            if e.errno == 98: # Address already in use
+                continue
+            raise
 
 
 if __name__ == "__main__":
